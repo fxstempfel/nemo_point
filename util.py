c98db65c0c9e3689932872868914c632c57e46ce
Fxstempfel <fxstempfel@gmail.com>
2026-02-13 11:57:08 +0100

    feat: add target_name to resources and output names

--- util.py
+++ util.py
@@ -11,6 +11,8 @@ def normalized_name(name: str) -> str:
     return name.lower().replace(" ", "_")
 
 
-OSM_REGION_PATH = pathlib.Path(CONFIG["paths"]["resources"]) / f"region_{normalized_name(CONFIG["osm_region_name"])}.wkt"
-OSM_POINTS_PATH = pathlib.Path(CONFIG["paths"]["resources"]) / f"points_{normalized_name(CONFIG["osm_region_name"])}.wkt"
-MAP_OUTPUT_PATH = pathlib.Path(CONFIG["paths"]["output"]) / f"map_{normalized_name(CONFIG["osm_region_name"])}.html"
+_target_name = normalized_name(CONFIG["target_name"])
+_region_name = normalized_name(CONFIG["osm_region_name"])
+OSM_REGION_PATH = pathlib.Path(CONFIG["paths"]["resources"]) / f"region_{_region_name}.wkt"
+OSM_POINTS_PATH = pathlib.Path(CONFIG["paths"]["resources"]) / f"points_{_target_name}_{_region_name}.wkt"
+MAP_OUTPUT_PATH = pathlib.Path(CONFIG["paths"]["output"]) / f"map_{_target_name}_{_region_name}.html"
